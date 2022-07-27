57d93d06f4650d0e834ebb05f19c1e58def6648d
Strift <laurent@twic.pics>
2022-07-27 23:40:06 +0200

    Install TailwindCSS

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import '@/assets/base.css'
+
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="max-w-screen-lg mx-auto px-5">
+    <!-- <NuxtPage /> -->
   </div>
+  <footer class="bg-gray-900 py-10 mt-24 px-5">
+    <div class="max-w-screen-sm mx-auto text-center text-lg text-gray-100">
+      Built with 🧡 for the Vue.js Forge workshop.
+    </div>
+  </footer>
 </template>
